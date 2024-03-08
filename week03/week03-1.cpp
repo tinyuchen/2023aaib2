5fcd08dcb5f6ffbf85191fa888ce2b9d22741d68
tinyuchen <tychen.grace@gmail.com>
2024-03-08 11:43:18 +0800

    week03

---
+++ week03/week03-1.cpp
@@ -0,0 +1,10 @@
+///week03-1.cpp
+#include <stdio.h>
+int main()
+{
+    int a=5;
+    ///if(a%2==1) printf("odd\n");
+    while(a%2==1) printf("odd\n");
+    ///if(a>0) printf("a is >0\n");
+    while(a>0) printf("a is >0\n");
+}
---
+++ week03/week03-2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a=5;
+    while(a>0){
+        printf("a is %d\n",a);
+        a-=1;
+    }
+}
