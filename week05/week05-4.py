ea1f98ab25d4bab72931c4b85e5a756ba725838a
tinyuchen <tychen.grace@gmail.com>
2024-03-22 12:05:13 +0800

    week05

---
+++ week05/week05-4.py
@@ -0,0 +1,12 @@
+#week05-4.py
+#將week05-1.cpp 的code translate to python
+
+a=[0]*4
+b=[10,20,30,40]
+a[0]=9
+a[1]=8
+a[2]=7
+a[3]=6
+for i in range(4):
+  print("i:",i,"a[i]:",a[i],"b[i]:",b[i])
+#Paste the code to python tutor
