bc51a73c2f7d5cecc7175284fb2aabc037517ab8
tinyuchen <tychen.gracegmail.com>
2024-03-29 11:50:24 +0800

    week06

---
+++ week06/week06-1a.cpp
@@ -0,0 +1,13 @@
+///<畫星星>
+///從9x9乘法表開始
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1; i<=n;i++){
+        printf("i:%d\n",i);
+    }
+}
---
+++ week06/week06-1b.cpp
@@ -0,0 +1,16 @@
+///<畫星星>
+///從9x9乘法表開始
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1; i<10;i++){
+        for(int j=1;j<=n;j++){
+            printf("*");
+        }
+        printf("i:%d\n",i);
+    }
+}
