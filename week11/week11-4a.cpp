dbad5407e304ac8ee0c032578b8af06f19ce638b
tinyuchen <tychen.grace@gmail.com>
2024-05-03 11:54:48 +0800

    week11

---
+++ week11/week11-4a.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a[10][10];
+	int m,n;
+	scanf("%d%d",&m,&n);
+	///part1: Input
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+}
